1664779dc6f81c17764b8c439a266bd34434b01b
wickdeeb <isak.m.h.s@gmail.com>
2020-07-04 12:21:22 +0200

    Add styling and split html js and css files

---
+++ index.js
@@ -0,0 +1,24 @@
+function handleChange(evt) {
+  const input = document.getElementById('img-upload');
+  const reader = new FileReader();
+
+  reader.readAsDataURL(input.files[0]);
+  reader.onloadend = function() {
+    var base64data = reader.result;
+    if (typeof base64data === 'string') {
+      postImage(base64data);
+    }
+  };
+}
+
+function postImage(img) {
+  fetch('http://localhost:3001/create', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ data: img })
+  })
+    .then(res => console.log(res.status))
+    .catch(error => console.log(error));
+}
