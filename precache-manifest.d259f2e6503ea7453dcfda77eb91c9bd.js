90c6ea179757dfcefa3ea929dd244854bc6fe374
wickdeeb <isak.m.h.s@gmail.com>
2020-07-13 21:50:02 +0200

    Update

---
+++ precache-manifest.d259f2e6503ea7453dcfda77eb91c9bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15f1a4eef4253d73b09a4a73613f35e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19b7a8b7be974796e89d",
+    "url": "/static/css/main.e6c7a14a.chunk.css"
+  },
+  {
+    "revision": "903e61b5d6b93e7e5cf4",
+    "url": "/static/js/2.b88d91a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b88d91a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19b7a8b7be974796e89d",
+    "url": "/static/js/main.27ffab3c.chunk.js"
+  },
+  {
+    "revision": "66e7929d964f773f5927",
+    "url": "/static/js/runtime-main.69befa29.js"
+  }
+]);
